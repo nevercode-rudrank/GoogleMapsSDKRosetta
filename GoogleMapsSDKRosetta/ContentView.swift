da0a51aafe023cb67ada50a61dd1466963091926
Rudrank Riyam <rudrankriyam@gmail.com>
2023-02-16 11:34:29 +0530

    Initial commit

---
+++ GoogleMapsSDKRosetta/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoogleMapsSDKRosetta
+//
+//  Created by Rudrank Riyam on 16/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoogleMapsSDKRosetta/GoogleMapsSDKRosettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapsSDKRosettaApp.swift
+//  GoogleMapsSDKRosetta
+//
+//  Created by Rudrank Riyam on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMapsSDKRosettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
